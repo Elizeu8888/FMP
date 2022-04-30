0d660067b82db2c4d89e411a21ddec00423978df
Elizeu8888 <527603@chrome.escg.ac.uk>
2022-04-30 21:13:16 +0000

    added weapon draw and animations

---
+++ FMP/Assets/Scripts/EventSwitch.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class EventSwitch : MonoBehaviour
+{
+    public UnityEvent use;
+    public UnityEvent altUse;
+    public UnityEvent altUse2;
+    public UnityEvent altUse3;
+    public UnityEvent altUse4;
+    public UnityEvent altUse5;
+    public UnityEvent altUse6;
+    public UnityEvent damage;
+    //public SoundManager soundscript;
+
+    public void Damage()
+    {
+        damage.Invoke();
+    }
+    public void Use()
+    {
+        use.Invoke();
+    }
+
+    public void AltUse()
+    {
+        altUse.Invoke();
+    }
+
+    public void AltUse2()
+    {
+        altUse2.Invoke();
+    }
+
+    public void AltUse3()
+    {
+        altUse3.Invoke();
+    }
+
+    public void AltUse4()
+    {
+        altUse4.Invoke();
+    }
+    public void AltUse5()
+    {
+        altUse5.Invoke();
+    }
+    public void AltUse6()
+    {
+        altUse6.Invoke();
+    }
+}
